33606b53f4ffa5e05a4294526b4b57c130865c77
Suiseich <aarontayag89@gmail.com>
2025-10-15 11:38:10 +0800

    Added Functions to Edit Page

--- pages/edituserpage.js
+++ pages/edituserpage.js
@@ -1,9 +1,70 @@
-import {View, Text} from 'react-native';
+import { View, Text, Button, TextInput } from "react-native";
+import { useState } from "react";
+import axios from "axios";
+import styles from "../styles";
+export default function EditUserPage({ route }) {
+  const { user } = route.params;
 
-export default function EditUserPage() {
-    return (
-        <View>
-            <Text>Edit User</Text>
-        </View>
-    )
+  const [first_Name, setFirstName] = useState(user.first_name);
+  const [last_Name, setLastName] = useState(user.last_name);
+  const [email, setEmail] = useState(user.email);
+  const [gender, setUserGender] = useState(user.gender);
+  const [password, setPassword] = useState(user.password);
+
+  const handleUpdate = () => {
+    if (!first_Name || !last_Name || !email || !gender || !password) {
+      window.alert("Error", "Please fill up all required fields");
+      return;
+    }
+    axios
+      .put(`http://172.19.128.1:8000/registration/api/users/${user.id}/`, {
+        first_name: first_Name,
+        last_name: last_Name,
+        email: email,
+        gender: gender,
+        password: password,
+      })
+      .then(() => {
+        window.alert("Success", "User updated successfully");
+      })
+      .catch((error) => {
+        console.error(error);
+        window.alert("Error", "Failed to update user!");
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText2}>Edit User</Text>
+      <TextInput
+        style={styles.input}
+        value={first_Name}
+        onChangeText={setFirstName}
+      />
+
+      <TextInput
+        style={styles.input}
+        value={last_Name}
+        onChangeText={setLastName}
+      />
+
+      <TextInput style={styles.input} value={email} onChangeText={setEmail} />
+
+      <TextInput
+        style={styles.input}
+        value={gender}
+        onChangeText={setUserGender}
+      />
+
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button title="Update Record" onPress={handleUpdate} />
+      </View>
+    </View>
+  );
 }
